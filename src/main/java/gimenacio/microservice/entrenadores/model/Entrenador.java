2f6bd8fe227d80f480c4d5f7e42394b25da11617
Duvan R. Cuero <47835668+DuvanRCuero@users.noreply.github.com>
2025-02-24 10:23:10 -0500

    Data loader added

---
+++ src/main/java/gimenacio/microservice/entrenadores/model/Entrenador.java
@@ -0,0 +1,23 @@
+package gimenacio.microservice.entrenadores.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Entrenador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String especialidad;
+    private Double calificacion;
+    private Boolean disponible;
+
+
+
+}
